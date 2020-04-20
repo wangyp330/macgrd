dcfdc4e0ec724dedcee4d6debb7b0253ced1a442
cloudypwang <cloudypwang@tencent.com>
2020-04-20 17:36:57 +0800

    ddddd

---
+++ build_appstore.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sh ./build.sh -c AppStore
+
---
+++ mytracy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mytracy
+//
+//  Created by cloudypwang on 2020/4/20.
+//  Copyright © 2020 cloudypwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.fuck(firstad: "fuck", second: 1);
+    }
+
+    func fuck (firstad : NSString , second : NSInteger){
+        
+        print(firstad,"hahah");
+    }
+
+}
+
