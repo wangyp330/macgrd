5a44d913a304006b024e1a7e6f5e38c1107f8bd3
cloudypwang <cloudypwang@tencent.com>
2020-05-13 18:36:38 +0800

    hebddddd

---
+++ mytracy/fitViewController.swift
@@ -0,0 +1,30 @@
+//
+//  fitViewController.swift
+//  mytracy
+//
+//  Created by cloudypwang on 2020/5/13.
+//  Copyright © 2020 cloudypwang. All rights reserved.
+//
+
+import UIKit
+
+class fitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
